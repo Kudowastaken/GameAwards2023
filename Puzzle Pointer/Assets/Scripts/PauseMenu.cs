bdda2d63eaa2c73ba4aa242fd9e37dfce05b404d
Ministar200 <128044449+Ministar200@users.noreply.github.com>
2023-04-05 14:47:05 +0200

    PauseMenu

---
+++ Puzzle Pointer/Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : SingletonPersistent<PauseMenu>
+{
+    [SerializeField] bool isPaused;
+    GameObject pauseMenu;
+    private void Start()
+    {
+        pauseMenu = GameObject.FindGameObjectWithTag("PauseMenu");
+        pauseMenu.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isPaused == false && Input.GetKeyDown(KeyCode.Escape))
+        {
+            pauseMenu.SetActive(true);
+            isPaused = true;
+        }
+        else if (isPaused == true && Input.GetKeyDown(KeyCode.Escape))
+        {
+            pauseMenu.SetActive(false);
+            isPaused = false;
+        }
+    }
+}
