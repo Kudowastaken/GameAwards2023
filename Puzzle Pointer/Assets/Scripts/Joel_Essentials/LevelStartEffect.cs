f8871a81aa51694a992ea1853c0b2c58faaef144
Ministar200 <128044449+Ministar200@users.noreply.github.com>
2023-04-21 19:14:58 +0200

    PauseMenu + lvlstarteffect

---
+++ Puzzle Pointer/Assets/Scripts/Joel_Essentials/LevelStartEffect.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Dumt namn men tidspress
+public class LevelStartEffect : MonoBehaviour
+{
+    [SerializeField] Transform[] affectedTransforms = new Transform[0];
+
+    [SerializeField] float startPositionRadius = 15f;
+    [SerializeField] float startPositionHeightIncrease;
+    [SerializeField] float startPositionXAxis;
+
+    [SerializeField] float minDuration, maxDuration;
+    [SerializeField] AnimationCurve movementCurve;
+    
+    void Start()
+    {
+        for (int i = 0; i < affectedTransforms.Length; i++)
+        {
+            StartCoroutine(MoveObjectToPosition(affectedTransforms[i], Random.Range(minDuration, maxDuration)));
+        }
+    }
+
+    IEnumerator MoveObjectToPosition(Transform objTransform, float movementDuration)
+    {
+        Vector2 targetPosition = objTransform.position;
+        Vector2 startPosition = objTransform.position = Quaternion.AngleAxis(Random.Range(0, 360), Vector3.forward) * (Vector2.right * startPositionRadius);
+
+        startPosition = new Vector2(startPosition.x + startPositionXAxis, startPosition.y + startPositionHeightIncrease);
+
+        foreach (Collider2D objCollider in objTransform.GetComponentsInChildren<Collider2D>())
+        {
+            objCollider.enabled = false;
+        }
+
+        objTransform.position = startPosition;
+
+        float t = 0f;
+        while (t < 1f)
+        {
+            t += Time.deltaTime / movementDuration;
+            
+            objTransform.position = Vector2.LerpUnclamped(startPosition, targetPosition, movementCurve.Evaluate(t));
+
+            yield return new WaitForEndOfFrame();
+        }
+
+        objTransform.position = targetPosition;
+        foreach (Collider2D objCollider in objTransform.GetComponentsInChildren<Collider2D>())
+        {
+            objCollider.enabled = true;
+        }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(Vector2.zero, startPositionRadius);
+    }
+}
